ca768d20f35173ce3656b2060eb65c0bdff90598
ErikaBernabe <erikayazmin.27@gmail.com>
2023-05-23 20:23:09 -0500

    23/05/23

--- Parcial 3/ServidorExpress copy/constExpress.js
+++ Parcial 3/ServidorExpress copy/constExpress.js
@@ -10,51 +10,71 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-//CONSULTA
-app.get('/cliente', (req, res)=>{
-    // app.get('/cliente/:id_cliente', (req, res)=>{
-    
-    //Para validar si el cliente puso un id para buscar el dato
-    if(typeof(req.query.Id)=== `undefined`){
-        res.json({mensaje: "Debe de enviar el parametro Id EN LA CADENA DE CONSULTA"})
 
-    }else{
-
-        
-    //var   
+ //Mostrar Toda la tabla
+ app.get('/pelicula', (req, res) => {
+  
     const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password : '',
-        database: 'peliculas'
-
+          host: 'localhost',
+          user: 'root',
+          password: '',
+          database: 'peliculas'
     });
-    connection.connect();
-    connection.query(`SELECT * FROM pelicula WHERE Id = ${req.query.Id}`,function(error, results,fields){
 
-    //para validar si el id que pusierin no esta, si esta mal la forma de la consulta en la linea 26, si no pueron id
-     if(error){
-         res.json(error);
+      connection.connect();
+            
+      connection.query(`SELECT * FROM pelicula `, function (error, results, fields) {
+          if (error) {
+              res.json(error);
+          } else {
+              if (results.length === 0) {
+                  res.json({ Mensaje: "Pelicula no encontrada" });
+              } else {
+                  res.json(results);
+              }
+          }
+      });
 
-     }else{
-        if(results.length==0){
-            res.json({mensaje: "Id de la Pelicula no encontrada"});
+      connection.end();
+  
+});
 
-        }else{
-            res.json(results);
-
-        }
-    }
-
-    });
-    connection.end();
-
-  }
-})
+//CONSULTA2 Mostrar solo uno con el Id
+app.get('/pelicula/:Id', (req, res) => {
+    
+    //podria ser req.params.id    tambien con .body.id    query.id
+      if (typeof(req.params.Id) === 'undefined') {
+          res.json({ estado:0,
+          resultado: "Debe enviar el ID del género" });
+      } else {
+          const connection = mysql.createConnection({
+              host: 'localhost',
+              user: 'root',
+              password: '',
+              database: 'peliculas'
+          });
+  
+          connection.connect();
+                                                         //podria ser req.params.id   tambine con req.body.id    
+          connection.query(`SELECT * FROM pelicula WHERE Id=${req.params.Id}`, function (error, results, fields) {
+              if (error) {
+                  res.json({estado:0, resultado:error.sqlMessage});
+              } else {
+                  if (results.length === 0) {
+                      res.json({estado:0, resultado: "pelicula no encontrada" });
+                  } else {
+                      res.json({estado:1, resultado:results[0]});
+                  }
+              }
+          });
+  
+          connection.end();
+      }
+  });
 
 
 //BORRAR
-app.delete('/cliente/:Id', (req, res)=>{
+app.delete('/pelicula/:Id', (req, res)=>{
     // app.get('/cliente/:id_cliente', (req, res)=>{
     
     //Para validar si el cliente puso un id para buscar el dato
@@ -79,7 +99,7 @@ app.delete('/cliente/:Id', (req, res)=>{
 
      }else{
         if(results.affectedRows==1){
-            res.json({estado:1,resultado:"Cliente borrado"});
+            res.json({estado:1,resultado:"pelicula borrada"});
 
         }else{
             res.json({estado:0, resultado:"ocurrio un error en la eliminacion"});
@@ -94,7 +114,7 @@ app.delete('/cliente/:Id', (req, res)=>{
 })
 
 //ALTA
-app.post('/cliente', (req, res)=>{
+app.post('/pelicula', (req, res)=>{
    
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -103,7 +123,7 @@ app.post('/cliente', (req, res)=>{
         database: 'peliculas'
 
     });
-   
+      
     let sentenciasql="insert into pelicula values("+req.body.Id+","+
                                                 "'"+req.body.Nombre+"',"+
                                                 "'"+req.body.Genero+"',"+
@@ -163,3 +183,172 @@ app.listen(8082, (req, res)=>{
 
 
 //este tendra el  se dede instalar en el servidor express npm install cors
+
+
+
+
+// // const { error } = require('console');
+// const express = require('express');
+// const app = express();
+// const mysql = require('mysql');
+// const cors =require('cors');
+// const bodyParser = require('body-parser');
+
+// // app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(cors());
+
+
+// //CONSULTA
+// app.get('/cliente', (req, res)=>{
+//     // app.get('/cliente/:id_cliente', (req, res)=>{
+    
+//     //Para validar si el cliente puso un id para buscar el dato
+//     if(typeof(req.query.Id)=== `undefined`){
+//         res.json({mensaje: "Debe de enviar el parametro Id EN LA CADENA DE CONSULTA"})
+
+//     }else{
+
+        
+//     //var   
+//     const connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password : '',
+//         database: 'peliculas'
+
+//     });
+//     connection.connect();
+//     connection.query(`SELECT * FROM pelicula WHERE Id = ${req.query.Id}`,function(error, results,fields){
+
+//     //para validar si el id que pusierin no esta, si esta mal la forma de la consulta en la linea 26, si no pueron id
+//      if(error){
+//          res.json(error);
+
+//      }else{
+//         if(results.length==0){
+//             res.json({mensaje: "Id de la Pelicula no encontrada"});
+
+//         }else{
+//             res.json(results);
+
+//         }
+//     }
+
+//     });
+//     connection.end();
+
+//   }
+// })
+
+
+// //BORRAR
+// app.delete('/cliente/:Id', (req, res)=>{
+//     // app.get('/cliente/:id_cliente', (req, res)=>{
+    
+//     //Para validar si el cliente puso un id para buscar el dato
+//     if(typeof(req.params.Id)=== `undefined`){
+//         res.json({estado:0,
+//                    resultado:"debe de enviar el parametro Id en la cadena de consulta"})
+
+//     }else{ 
+//     var connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password : '',
+//         database: 'peliculas'
+
+//     });
+//     connection.connect();
+//     connection.query(`delete  from pelicula where Id = ${req.params.Id}`,function(error, results,fields){
+
+//     //para validar si el id que pusierin no esta, si esta mal la forma de la consulta en la linea 26, si no pueron id
+//      if(error){
+//          res.json({estado:0,resultado:error.sqlMessage});
+
+//      }else{
+//         if(results.affectedRows==1){
+//             res.json({estado:1,resultado:"Cliente borrado"});
+
+//         }else{
+//             res.json({estado:0, resultado:"ocurrio un error en la eliminacion"});
+
+//         }
+//     }
+
+//     });
+//     connection.end();
+
+//   }
+// })
+
+// //ALTA
+// app.post('/cliente', (req, res)=>{
+   
+//     var connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password : '',
+//         database: 'peliculas'
+
+//     });
+   
+//     let sentenciasql="insert into pelicula values("+req.body.Id+","+
+//                                                 "'"+req.body.Nombre+"',"+
+//                                                 "'"+req.body.Genero+"',"+
+//                                                 "'"+req.body.FechaEstreno+"',"+
+//                                                 "'"+req.body.Duracion+"'"+")";
+
+//      console.log(sentenciasql);
+
+//      connection.connect();                                           
+
+//     connection.query(sentenciasql,function(error, results,fields){
+
+//      if(error){
+//          res.json(error);
+
+//      }else{
+//        console.log(results);
+//        res.json(results);
+//     }
+
+//     });
+//     connection.end();
+
+  
+// })
+
+
+
+
+
+
+
+
+
+
+
+// app.post('/', (req, res)=>{
+//     res.json({respuesta:"Respuesta a peticion get"})
+// })
+
+
+
+// app.listen(8082, (req, res)=>{
+// })
+//     console.log('Servidor express escuchando en puerto 8082')
+
+
+
+// // $npm init -y
+
+// // npm install express creo que esto se intala primero
+
+// // $npm i express
+// //       --save-dex
+
+// // npm i mysqual para conectar con mysqual
+
+
+// //este tendra el  se dede instalar en el servidor express npm install cors
