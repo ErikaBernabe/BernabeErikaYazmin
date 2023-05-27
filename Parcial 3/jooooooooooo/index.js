f2a71375f0063599188828c5fe2ca7cb75d23283
ErikaBernabe <erikayazmin.27@gmail.com>
2023-05-27 00:01:23 -0500

    27-05-2023

--- Parcial 3/jooooooooooo/index.js
+++
@@ -1,7 +0,0 @@
-new gridjs.Grid({
-    columns:['Id','Nombre','Genero','FechaEstreno',Duracion],
-    server:{
-        url:'http://localhost:8082/pelicula',
-        then: data=> data.map(pelicula=>[pelicula.Id,pelicula.Nombre,pelicula.Genero,pelicula.FechaEstreno, pelicula.Duracion])
-    }
-}).render(document.getElementById("wrapper"))
